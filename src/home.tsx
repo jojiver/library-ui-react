1767bbfd746d1acfaf8b4136141be9de35cf252a
jojiver <jansumalinog0@gmail.com>
2026-09-06 21:42:55 +0800

    First commit

---
+++ src/home.tsx
@@ -0,0 +1,7 @@
+
+export default function HomePage(){ 
+  return(
+
+     <h1 className="text-green-600">Hello World</h1>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import './styles/global.css';
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from './home.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+
+    
+    </Routes>
+  </BrowserRouter>
+  
+)
